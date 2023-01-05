fd8091e9f834bf63c1b6622aa445de9044ad576a
Dev Sales <devrsales@gmail.com>
2023-01-05 00:41:39 -0300

    Clean Arch - Validação de Products

--- src/domain/customer/entity/customer.ts
+++ src/domain/customer/entity/customer.ts
@@ -1,5 +1,6 @@
 import Entity from "../../@shared/entity/entity.abstract";
 import NotificationError from "../../@shared/notification/notification.error";
+import CustomerValidatorFactory from "../factory/customer.validator.factory";
 import Address from "../value-object/Address";
 
 export default class Customer extends Entity{
@@ -33,19 +34,7 @@ export default class Customer extends Entity{
     }
     
     validate(){
-        if(this.id.length === 0) {
-            this.notification.addError({
-                context: "customer",
-                message: "Id is required"
-            });
-        }
-
-        if(this._name.length === 0) {
-            this.notification.addError({
-                context: "customer",
-                message: "Name is required"
-            });
-        }
+        CustomerValidatorFactory.create().validate(this);
 
         if(this.notification.hasErrors()) {
             throw new NotificationError(this.notification.getErrors());
---
+++ src/domain/product/validator/product.yup.validator.ts
@@ -0,0 +1,34 @@
+import ValidatorInterface from "../../@shared/validator/validator.interface";
+import Product from "../entity/product";
+import * as yup from "yup";
+import ProductInterface from "../entity/product.interface";
+
+export default class ProductYupValidator implements ValidatorInterface<ProductInterface> {
+    validate(entity: ProductInterface): void {
+       try {
+            yup
+            .object()
+            .shape({
+                id: yup.string().required("Id is required"),
+                name: yup.string().required("Name is required"),
+                price: yup.number().moreThan(0 ,"Price must be greater than 0"),
+            })
+            .validateSync({
+                id: entity.id,
+                name: entity.name,
+                price: entity.price,
+            },{
+                abortEarly: false,
+            })
+       } catch (errors) {
+            const e = errors as yup.ValidationError;
+            e.errors.forEach((error) => {
+                entity.notification.addError({
+                    context: "product",
+                    message: error,
+                });
+            });
+       }
+    }
+    
+}
